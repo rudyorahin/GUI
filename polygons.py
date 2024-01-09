cb29ec45838255ac4d7fa4ff09da6eb212a19544
rudyorahin <55644998+rudyorahin@users.noreply.github.com>
2024-01-08 22:35:16 -0800

    Create polygons.py

---
+++ polygons.py
@@ -0,0 +1,24 @@
+# polygons.py
+def square(t, length):
+    """Draws a square with the given length."""
+    for count in range(4):
+        t.forward(length)
+        t.left(90)
+
+def hexagon(t, length):
+    """Draws a hexagon with the given length."""
+    for count in range(6):
+        t.forward(length)
+        t.left(60)
+
+def radialHexagons(t, n, length):
+    """Draws a radial pattern of n hexagons with the given length."""
+    for count in range(n):
+        hexagon(t, length)
+        t.left(360 / n)
+
+def radialPattern(t, n, length, shape):
+    """Draws a radial pattern of n shapes with the given length."""
+    for count in range(n):
+        shape(t, length)
+        t.left(360 / n)
